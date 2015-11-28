35528c4edd534d2d8e9d52a55b2e6059088490aa
Ian Strasser <ianstrasser@protonmail.com>
2015-11-28 16:46:00 -0500

    misc

--- GooberApp/app/src/main/java/ianstrasser/gooberapp/BrushTeethActivity.java
+++ GooberApp/app/src/main/java/ianstrasser/gooberapp/BrushTeethActivity.java
@@ -2,15 +2,20 @@ package ianstrasser.gooberapp;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
-public class BrushTeethActivity extends Activity {
+public class BrushTeethActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_brush_teeth);
+        //Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setActionBar(myToolbar);
+
     }
 
     public void dayBrush(View v) {
